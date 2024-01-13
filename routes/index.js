74b0d7bf5b8f9e858f37bc2df7a2ac07021ac39a
syerif elfaz <syerif@gmail.com>
2024-01-13 07:43:51 +0700

    Entod

--- routes/index.js
+++ routes/index.js
@@ -13,32 +13,32 @@ router.get('/', function (req, res,next) {
     next();
 })
 
-// router.get('/api/pattern',function (req, res) {
-//     res.send({message:'Welcome Patern'})
-//     res.end();
-// })
+router.get('/api/pattern',function (req, res) {
+    res.send({message:'Welcome Patern'})
+    res.end();
+})
 
-// router.get('/api/pattern/latest_status/:sclId',Auth.authAccessToken,function (req, res){
-//     Assets.LatestStatus(req,res)
-// })
+router.get('/api/pattern/latest_status/:sclId',Auth.authAccessToken,function (req, res){
+    Assets.LatestStatus(req,res)
+})
 
-// router.get('/api/pattern/vehicle_user',Auth.authAccessToken,function (req, res){
-//     Vehicle_User.Read(req,res)
-// })
+router.get('/api/pattern/vehicle_user',Auth.authAccessToken,function (req, res){
+    Vehicle_User.Read(req,res)
+})
 
-// router.get('/api/pattern/vehicle_user/:userid',Auth.authAccessToken,function (req, res){
-//     Vehicle_User.ReadVehicleByUser(req,res)
-// })
+router.get('/api/pattern/vehicle_user/:userid',Auth.authAccessToken,function (req, res){
+    Vehicle_User.ReadVehicleByUser(req,res)
+})
 
-// router.get('/api/pattern/asset_address/:lat/:lng',Auth.authAccessToken,function (req, res){
-//     Assets.AssetAddress(req,res)
-// })
+router.get('/api/pattern/asset_address/:lat/:lng',Auth.authAccessToken,function (req, res){
+    Assets.AssetAddress(req,res)
+})
 
 
 
-// router.post('/api/patern/auth',function (req, req) {
-//     Auth.Login(req,res)
-// })
+router.post('/api/patern/auth',function (req, req) {
+    Auth.Login(req,res)
+})
 
 module.exports = router
 
