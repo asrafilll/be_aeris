bdd3a5f2774233844786d414f2e6b9b06e801157
asrafilll <asrafilr@gmail.com>
2024-04-24 20:57:48 +0700

    serve public folder

--- indexMobile.js
+++ indexMobile.js
@@ -1,47 +1,61 @@
-var express = require ('express')
-var bodyParser = require('body-parser');
-var util = require('util');
-var cors = require('cors');
-var futil = require('./config/utility.js');
-var con = require ('./config/database.js');
-var routes = require('./routes/index.js');
+var express = require("express");
+var bodyParser = require("body-parser");
+var util = require("util");
+var cors = require("cors");
+var futil = require("./config/utility.js");
+var con = require("./config/database.js");
+var routes = require("./routes/index.js");
 const path = require("path");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 // var devices = require('./controllers/device.js')
-require('dotenv').config();
+require("dotenv").config();
 
-var app = express()
-
+var app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.options('*', cors())
+app.options("*", cors());
 const corsOptions = {
-    exposedHeaders: 'token',
-  };
+  exposedHeaders: "token",
+};
 app.use(cors(corsOptions));
 
 app.use(fileUpload());
-app.use(routes.router)
-
+app.use(routes.router);
+app.use(express.static("public"));
 
 var server = app.listen(process.env.SERVER_PORT, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    
+  var host = server.address().address;
+  var port = server.address().port;
 
-    futil.logger.debug('\n' + futil.shtm() + '- [ W A K E   U P ] | STARTING ' + util.inspect(process.env.TITLE));
-    futil.logger.debug(futil.shtm() + '- [ W A K E   U P ] | RUN AT PATH: /api/pattern, PORT: ' + port);
+  futil.logger.debug(
+    "\n" +
+      futil.shtm() +
+      "- [ W A K E   U P ] | STARTING " +
+      util.inspect(process.env.TITLE)
+  );
+  futil.logger.debug(
+    futil.shtm() +
+      "- [ W A K E   U P ] | RUN AT PATH: /api/pattern, PORT: " +
+      port
+  );
 
-    // Testing database connection 
-    try {
-        con.db.authenticate();
-        futil.logger.debug('\n' + futil.shtm() + '- [ DATABASE U P ] | STARTING ' + util.inspect(process.env.DATABASE));
-    } catch (error) {
-        futil.logger.debug('\n' + futil.shtm() + '- [ DATABASE ERROR] | STARTING ' + util.inspect(error));
-    }
-
-
-
+  // Testing database connection
+  try {
+    con.db.authenticate();
+    futil.logger.debug(
+      "\n" +
+        futil.shtm() +
+        "- [ DATABASE U P ] | STARTING " +
+        util.inspect(process.env.DATABASE)
+    );
+  } catch (error) {
+    futil.logger.debug(
+      "\n" +
+        futil.shtm() +
+        "- [ DATABASE ERROR] | STARTING " +
+        util.inspect(error)
+    );
+  }
 });
